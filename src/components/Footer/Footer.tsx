da5fff69f090ef682978ed893e04d6fefc5a3cd5
Vladymyr <vladymyr.svyrydenko2004@gmail.com>
2024-07-30 11:33:15 +0300

    small fixes

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,35 +1,33 @@
 import { useMediaQuery } from 'react-responsive'
 import Logo from '../../assets/icons/logo.png'
-import Email from '../../assets/icons/email.svg'
 import Phone from '../../assets/icons/phone.svg'
-
 import s from './Footer.module.css'
 
 const Footer = () => {
   const isMobile = useMediaQuery({ maxWidth: 767 })
-  // footer container flex-around items-start w-full md:flex-row flex-col p-20 gap-[60px] md:gap-[140px] overflow-hidden
+  
   return (
-    <div className={s.footer}>
+    <><div className={s.footer}>
       <div className={s.footerContacts}>
         <div className={s.footerContentList}>
           <img src={Logo} width={127} height={28} alt="Logo" />
 
           <div className={s.footerContentItem}>
             <p className={s.footerContentTitle}>Наші контакти</p>
-            <p className={s.contacts}>
-              <img src={Email} width={17} height={16} alt="email" />
-              <a href="mailto:style.solution@gmail.com">style.solution@gmail.com</a>
-            </p>
+           
             <p className={s.contacts}>
               <img src={Phone} width={17} height={16} alt="phone" />
-              <a href="tel:+380 50 561 86 62">+380 50 561 86 62</a>
+              <a href="tel:+380 50 561 86 62">&nbsp;+380 50 561 86 62</a>
+            </p>
+            <p className={s.contacts}>
+            ФОП Свириденко Володимир Романович
             </p>
           </div>
         </div>
-        {!isMobile && <p className={s.subTitle}>Style Solution - місце, де клієнти отримують задоволення 🤤</p>}
+        {!isMobile && <p className={s.subTitle}><b>Style Solution</b> - сервіс, від котрого клієнти та бізнес отримують задоволення 🤤</p>}
       </div>
-      {!isMobile && <p className={s.legal}>© {new Date().getFullYear()} Style Solution</p>}
-    </div>
+
+    </div><br /></>
   )
 }
 
