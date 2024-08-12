80614368a586e6d52bafd2bc2c0d9c5525f62e5b
ArtemDid <temadidenko27@gmail.com>
2024-08-12 16:35:53 +0300

    changer form registration

--- src/components/RegistrationForm/RegistrationForm.tsx
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -1,72 +1,82 @@
-import React, { ChangeEvent, FormEvent, useState } from 'react'
-import validator from 'validator'
+import React, { useState } from 'react'
 import s from './RegistrationForm.module.css'
-import Button from '@mui/material/Button'
 import { useNavigate } from 'react-router-dom'
+import { Button, FormControl, TextField } from '@mui/material'
+import { MobileTimePicker } from '@mui/x-date-pickers/MobileTimePicker'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider'
+import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment'
+import moment from 'moment'
+import validator from 'validator'
 
 function filterEmptyFields(data: any) {
   return Object.fromEntries(Object.entries(data).filter(([key, value]) => (value as string).trim() !== ''))
 }
 
 export const RegistrationForm = () => {
+  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
+  const [error, setError] = useState<any>({
+    errorState: '',
+    errorName: '',
+    errorAddress: '',
+    errorPhone: '',
+    errorWebsite: '',
+    errorDescription: '',
+  })
   const navigate = useNavigate()
 
   const handleClick = () => {
     navigate('/thank-you-page')
   }
 
-  const [formData, setFormData] = useState({
-    name: '',
-    address: '',
-    phone: '',
-    website: '',
-    description: '',
-    opening_hours: '',
-  })
-
-  const [isSubmitting, setIsSubmitting] = useState<boolean>(false)
-  const [error, setError] = useState<string>('')
-  const [success, setSuccess] = useState<boolean>(false)
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    const { name, value } = e.target
-    setFormData({
-      ...formData,
-      [name]: value,
-    })
-  }
-
-  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: any) => {
     e.preventDefault()
     setIsSubmitting(true)
-    setError('')
-    setSuccess(false)
+    setError({
+      errorState: '',
+      errorName: '',
+      errorAddress: '',
+      errorPhone: '',
+      errorWebsite: '',
+      errorDescription: '',
+    })
 
-    const { name, address, phone, opening_hours, description } = formData
+    const formData = new FormData(e.currentTarget)
 
-    if (name.length < 2) {
-      setIsSubmitting(false)
-      return setError('Назва повинна бути не менше 2 символів')
+    const data = {
+      name: formData.get('name') as string,
+      address: formData.get('address') as string,
+      phone: formData.get('phone') as string,
+      description: formData.get('description') as string,
+      opening_hours: `${formData.get('startTime')} - ${formData.get('endTime')}`,
+      website: formData.get('website') as string,
     }
 
-    if (address.length < 2) {
+    if (data.name.length < 2) {
       setIsSubmitting(false)
-      return setError('Адреса повинна бути не менше 2 символів')
+      return setError({
+        errorName: 'Назва повинна бути не менше 2 символів',
+      })
     }
 
-    if (!validator.isMobilePhone(phone, 'uk-UA')) {
+    if (data.address.length < 2) {
       setIsSubmitting(false)
-      return setError('Введіть номер в правильному форматі: +380670000000 або 0670000000 ')
+      return setError({
+        errorAddress: 'Адреса повинна бути не менше 2 символів',
+      })
     }
 
-    if (description.length < 2) {
+    if (!validator.isMobilePhone(data.phone, 'uk-UA')) {
       setIsSubmitting(false)
-      return setError('Опис повинен бути не менше 2 символів')
+      return setError({
+        errorPhone: 'Введіть номер в правильному форматі: +380670000000 або 0670000000',
+      })
     }
 
-    if (opening_hours.length < 2) {
+    if (data.description.length < 2) {
       setIsSubmitting(false)
-      return setError('Введіть правильні часи роботи')
+      return setError({
+        errorDescription: 'Опис повинен бути не менше 2 символів',
+      })
     }
 
     try {
@@ -75,29 +85,23 @@ export const RegistrationForm = () => {
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify(filterEmptyFields(formData)),
+        body: JSON.stringify(filterEmptyFields(data)),
       })
 
-      const data = await response.json()
+      const responseData = await response.json()
 
-      if (data.hasOwnProperty('details')) {
+      if (responseData.hasOwnProperty('details')) {
         setIsSubmitting(false)
-        return setError(data.details[0].message)
+        return setError({
+          errorState: responseData.details[0].message,
+        })
       }
-
-      setSuccess(true)
-      setFormData({
-        name: '',
-        address: '',
-        phone: '',
-        website: '',
-        description: '',
-        opening_hours: '',
-      })
 
       handleClick()
     } catch (error) {
-      setError((error as any).message)
+      setError({
+        errorState: (error as any).message,
+      })
     } finally {
       setIsSubmitting(false)
     }
@@ -105,46 +109,81 @@ export const RegistrationForm = () => {
 
   return (
     <div className={s.registrationForm}>
-      {error && <p className={s.error}>{error}</p>}
-      {success && <p className={s.success}>Дані відправлені. Очікуйте, з вами зв'яжеться адміністратор.</p>}
+      {error.errorState && <p className={s.error}>{error.errorState}</p>}
       <form onSubmit={handleSubmit}>
-        <div>
-          <label>
-            Назва<span className={s.required}>*</span>:
-          </label>
-          <input type="text" name="name" value={formData.name} onChange={handleChange} required />
-        </div>
-        <div>
-          <label>
-            Адреса<span className={s.required}>*</span>:
-          </label>
-          <input type="text" name="address" value={formData.address} onChange={handleChange} required />
-        </div>
-        <div>
-          <label>
-            Телефон<span className={s.required}>*</span>:
-          </label>
-          <input type="tel" name="phone" value={formData.phone} onChange={handleChange} required />
-        </div>
-        <div>
-          <label>
-            Опис<span className={s.required}>*</span>:
-          </label>
-          <textarea name="description" value={formData.description} onChange={handleChange} required />
-        </div>
-        <div>
-          <label>
-            Часи роботи<span className={s.required}>*</span>:
-          </label>
-          <input type="text" name="opening_hours" value={formData.opening_hours} onChange={handleChange} required />
-        </div>
-        <div>
-          <label>Вебсайт:</label>
-          <input type="url" name="website" value={formData.website} onChange={handleChange} />
-        </div>
-        <Button variant="contained" size={'medium'} type="submit" disabled={isSubmitting}>
-          {isSubmitting ? 'Відправка...' : 'Зареєструватись'}
-        </Button>
+        <FormControl fullWidth>
+          <TextField
+            error={!!error.errorName}
+            id="outlined-error-helper-name"
+            name="name"
+            label="Назва"
+            helperText={error.errorName}
+            margin={'none'}
+            required={true}
+          />
+          <TextField
+            sx={{ marginTop: '20px' }}
+            error={!!error.errorAddress}
+            id="outlined-error-helper-address"
+            name="address"
+            label="Адреса"
+            helperText={error.errorAddress}
+            margin={'none'}
+            required={true}
+          />
+          <TextField
+            sx={{ marginTop: '20px' }}
+            error={!!error.errorPhone}
+            id="outlined-error-helper-phone"
+            name="phone"
+            label="Телефон"
+            type={'tel'}
+            helperText={error.errorPhone}
+            margin={'none'}
+            required={true}
+            placeholder={'+380670000000'}
+          />
+          <TextField
+            sx={{ marginTop: '20px' }}
+            error={!!error.errorDescription}
+            id="outlined-error-helper-description"
+            name="description"
+            label="Опис"
+            helperText={error.errorDescription}
+            margin={'none'}
+            required={true}
+            multiline
+          />
+          <LocalizationProvider dateAdapter={AdapterMoment}>
+            <MobileTimePicker
+              sx={{ marginTop: '20px' }}
+              name={'startTime'}
+              label={'Початок роботи'}
+              ampm={false}
+              defaultValue={moment('2022-04-17T09:00')}
+            />
+          </LocalizationProvider>
+          <LocalizationProvider dateAdapter={AdapterMoment}>
+            <MobileTimePicker
+              sx={{ marginTop: '20px' }}
+              name={'endTime'}
+              label={'Кінець роботи'}
+              ampm={false}
+              defaultValue={moment('2022-04-17T18:00')}
+            />
+          </LocalizationProvider>
+          <TextField
+            sx={{ marginTop: '20px' }}
+            type="url"
+            id="outlined-error-helper-url"
+            name="website"
+            label="Вебсайт"
+            margin={'none'}
+          />
+          <Button sx={{ marginTop: '20px' }} variant="contained" size={'medium'} type="submit" disabled={isSubmitting}>
+            {isSubmitting ? 'Відправка...' : 'Зареєструватись'}
+          </Button>
+        </FormControl>
       </form>
     </div>
   )
